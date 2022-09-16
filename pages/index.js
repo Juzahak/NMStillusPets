53a982e86dcf7efe333942c509a9ca47b4517857
Edevaldo Barboza <97263222+Juzahak@users.noreply.github.com>
2022-09-16 12:06:49 -0300

    att

--- pages/index.js
+++ pages/index.js
@@ -90,25 +90,29 @@ export default function Home({ admin }) {
         <PizzaList pizzaList={pizzaList || []} lista={lista || []} />
         <div className="container bg-white pt-5">
           <div className="col-12 d-flex flex-column">
-            <div className="col-12 col-lg-6 text-center">
+            <div className="col-12 col-lg-12 text-center">
               <Image src="/img/comomedir.jpg" alt="" width="500" height="500" />
             </div>
-            <div className="col-12 col-lg-6 d-flex text-center flex-column justify-content-center">
+            <div className="col-12 col-lg-12 d-flex text-center flex-column justify-content-center">
               <h2>Como medir seu pet?</h2>
               <p className="p-3">Escolher a roupa no tamanho correto é um fator importante para o conforto do animal. Tire as medidas antes de comprar para achar o look perfeito para seu pet! Depois encontre o tamanho ideal, através das medidas, nas tabelas dos tamanhos, de acordo com cada modelo.</p>
+              <div className="col-6 d-flex ">
               <div className={styles.btnsaiba}><a>Saiba Mais</a></div>
+              </div>
             </div>
           </div>
         </div>
         <div className="container bg-white mb-5">
           <div className="col-12 d-flex flex-column">
-            <div className="col-12 col-lg-6 text-center">
+            <div className="col-12 col-lg-12 text-center">
               <Image src="/img/vantagens.jpg" alt="" width="500" height="400" />
             </div>
-            <div className="col-12 col-lg-6 d-flex text-center flex-column justify-content-center">
+            <div className="col-12 col-lg-12 d-flex text-center flex-column justify-content-center">
               <h2>Vista seu pet</h2>
               <p className="p-3">Se você é o tipo de tutor que gosta de colocar roupas no seu pet, possivelmente já deve ter ouvido de alguém que se trata de frescura. Do mesmo modo, se você é um tutor que nunca recorre a esses acessórios, é bem possível que também já tenha ouvido que o pet pode estar com frio e que o melhor seria colocar nele um agasalho, não é mesmo?</p>
-              <div className={styles.btnsaiba}><a>Visite nosso Blog</a></div>
+               <div className="col-6 d-flex ">
+              <div className={styles.btnsaiba}><a>Saiba Mais</a></div>
+              </div>
             </div>
           </div>
         </div>
