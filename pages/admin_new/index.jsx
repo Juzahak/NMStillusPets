5e4453176eabb4d726487004c45472ccb51f73b3
Edevaldo Barboza <97263222+Juzahak@users.noreply.github.com>
2022-09-29 17:17:35 -0300

    att

--- pages/admin_new/index.jsx
+++ pages/admin_new/index.jsx
@@ -69,7 +69,7 @@ const Index = () => {
       <div className="col-lg-12 d-flex bg-white h-100">
         <Sidebar setClose2={setClose2} close2={close2} setClose3={setClose3} close3={close3} setClose4={setClose4} close4={close4}/>
 
-        {!close4 && <Pedidos setClose4={setClose4} close4={close4}/>}
+        {!close4 && <Finalizado setClose4={setClose4} close4={close4}/>}
         {!close3 && <Pedidos setClose3={setClose3} close3={close3}/>}
         {!close2 && <Produtos setClose2={setClose2} close2={close2}/>}
       </div>
